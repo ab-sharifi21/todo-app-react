3f6f25ea0ab812be65e7a649bdc1270bcf5c5293
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-05-16 19:11:23 +0200

    Change the over all styles of the application

--- src/App.tsx
+++ src/App.tsx
@@ -9,45 +9,47 @@ function App() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
 
-
   const doneTasks = tasks.filter((task) => {
     if (task.done) {
-      return task
+      return task;
     }
-  })
+  });
 
   const toDoTasks = tasks.filter((task) => {
     if (!task.done) {
-      return task
+      return task;
     }
-  })
+  });
 
   return (
-    <div className='flex flex-col items-center'>
+    <div className="flex flex-col items-center">
       <div className="my-4 bg-[#403D3933] shadow-box-shadow p-6 rounded-lg w-[350px]">
-          <AddTask tasks={tasks} setTasks={setTasks} />
+        <AddTask tasks={tasks} setTasks={setTasks} />
       </div>
 
-        <div className='flex gap-8 text-white'>
-          <div>
-            <h3 className='text-sm font-semibold my-2'>Tasks to do - {toDoTasks.length}</h3>
-            <TodoList
-              tasks={toDoTasks}
-              changeTaskDone={changeTaskDone}
-              deleteTask={deleteTask}
-            />
-          </div>
-
-          <div>
-          <h3 className='text-sm font-semibold my-2'>Done - {doneTasks.length}</h3>
-            <TodoList
-              tasks={doneTasks}
-              changeTaskDone={changeTaskDone}
-              deleteTask={deleteTask}
-            />
-          </div>
+      <div className="flex gap-8 text-white w-[90%]">
+        <div className="w-1/2">
+          <h3 className="text-sm font-semibold my-2">
+            Tasks to do - {toDoTasks.length}
+          </h3>
+          <TodoList
+            tasks={toDoTasks}
+            changeTaskDone={changeTaskDone}
+            deleteTask={deleteTask}
+          />
         </div>
 
+        <div className="w-1/2">
+          <h3 className="text-sm font-semibold my-2">
+            Done - {doneTasks.length}
+          </h3>
+          <TodoList
+            tasks={doneTasks}
+            changeTaskDone={changeTaskDone}
+            deleteTask={deleteTask}
+          />
+        </div>
+      </div>
     </div>
   );
 }
