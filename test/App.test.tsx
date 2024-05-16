0745849b8ad44b31936be0394590ba228fac1ace
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-05-16 19:02:16 +0200

    Delete the CI and tests from the project

--- test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
-import App from '../src/App';
-
-test('Should contain the correct title', () => {
-  render(<App />);
-  const title = screen.getByRole('heading');
-  expect(title).toHaveTextContent('Todo App');
-});
